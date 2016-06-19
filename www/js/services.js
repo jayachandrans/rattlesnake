cdabef3094bb5bee912add7f5ba867356041b845
jayachandrans <jayachandran.sivan@gmail.com>
2016-06-19 06:52:03 +0530

    Add files via upload

---
+++ www/js/services.js
@@ -0,0 +1,55 @@
+angular.module('starter.services', [])
+.service('LoginService', function ($q) {
+    return {
+        loginUser: function (name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function (fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function (fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+});
+
+/*
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
+*/
